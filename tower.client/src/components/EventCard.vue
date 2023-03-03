5921ea1bb527c006f90d16364200f11c296849e1
Jacob <Jacobzeme8@gmail.com>
2023-03-03 11:48:42 -0700

    border on cancelled events

--- tower.client/src/components/EventCard.vue
+++ tower.client/src/components/EventCard.vue
@@ -1,10 +1,9 @@
 <template>
-  <div v-if="event.creator" class="card my-3">
+  <div v-if="event.creator" class="card my-3" :class="`${event.isCanceled == true ? 'border-danger' : ''} `">
     <router-link :to="{ name: 'Event', params: { eventId: event.id } }">
       <img :src="event.coverImg" class="img-fluid cover-img mb-3" :title="`go to ${event.name}page`" alt="">
     </router-link>
     <h4>{{ event.name }}</h4>
-
     <p>Put together by: {{ event.creator.name }}</p>
     <p>Hosted at: {{ event.startDate }}</p>
   </div>
